c150144e075440f6cf53145bae2b200b8f1bd882
arathikini5 <arathikini7326@gmail.com>
2023-10-13 17:52:19 +0530

    Seventh commit

---
+++ DaySeven/src/com/cg/finalkeyword/Test.java
@@ -0,0 +1,14 @@
+package com.cg.finalkeyword;
+
+public class Test extends  ClassDemo {
+
+
+}
+//Final
+
+//1. Variable Restricts the modification
+
+//2. Method Restricts overriding
+
+//3. Class Restricts Inheritance
+
