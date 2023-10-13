27130c81b9971be0b681c9c6ef3b03d9d8f81a82
arathikini5 <arathikini7326@gmail.com>
2023-10-13 17:38:24 +0530

    First Commit

---
+++ Project1/src/Project1/Ternaryoperator.java
@@ -0,0 +1,15 @@
+package Project1;
+
+public class Ternaryoperator {
+		public static void main(String[] args) 
+		{
+			int x = 10;
+			
+			x = (10 == x) ? 1 : 0;
+			
+			System.out.println(x);
+	     
+		}
+
+
+}
---
+++ Project1/src/Project1/operator.java
@@ -0,0 +1,14 @@
+package Project1;
+
+public class operator {
+		public static void main(String[] args) 
+		{
+			int x = 4;
+			//x += 5; // x = x + 5
+			//x -= 5; // x = x - 5
+			//x *= 5; // x = x * 5
+			//x /= 4; // x = x / 4
+			x %= 5;   // x = x % 5
+			System.out.println(x);
+		}
+	}
